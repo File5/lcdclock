0276ac3e9200017adb3f52ae53bb701d811e8f81
Aleksandr Zuev <zuev08@gmail.com>
2022-01-17 16:35:42 +0100

    Add date

--- lcdclock/__main__.py
+++ lcdclock/__main__.py
@@ -209,14 +209,23 @@ def main():
     segments = Segment.ALL
     register(lcd, segments)
     prev_time = ''
+    prev_date = ''
+    f_time = ' %H %M %S '
+    f_date = '%a %d.%m.%Y'
     while True:
-        f = ' %H %M %S '
-        time_str = datetime.now().strftime(f)
+        now = datetime.now()
+        time_str = now.strftime(f_time)
+        date_str = now.strftime(f_date)
         if time_str != prev_time:
+            lcd.cursor_pos = (0, 0)
             s = Char4String.from_str(time_str)
             s.print(lcd)
             prev_time = time_str
-            lcd.cursor_pos = (0, 0)
+        if date_str != prev_date:
+            lcd.cursor_pos = (3, 0)
+            s = '{: ^20}'.format(date_str)
+            lcd.write_string(s)
+            prev_date = date_str
         time.sleep(0.1)
     lcd.close()
 
